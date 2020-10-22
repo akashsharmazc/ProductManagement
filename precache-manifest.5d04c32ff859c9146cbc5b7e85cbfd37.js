e901a283181596c72701c6bb0668eaeb1322b3ac
Akash Sharma <akash.sharmazc@gmail.com>
2020-10-23 03:45:49 +0530

    Add files via upload

---
+++ precache-manifest.5d04c32ff859c9146cbc5b7e85cbfd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa43459ac4e852e6d358e6d32eafbd5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc2f9c5c335b1cad2635",
+    "url": "/static/css/main.d2a872ea.chunk.css"
+  },
+  {
+    "revision": "93f99ef0c55103d287b3",
+    "url": "/static/js/2.df145b9b.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.df145b9b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc2f9c5c335b1cad2635",
+    "url": "/static/js/main.39237ea8.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
